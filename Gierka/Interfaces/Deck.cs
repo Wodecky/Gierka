7ebb8eb5471e25f65b12e585e6814284cd9c1299
Maciej Krebs <maciej.krebs@windowslive.com>
2018-06-26 18:13:46 +0200

    idck

---
+++ Gierka/Interfaces/Deck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gierka.Interfaces
+{
+    interface IDeck
+    {
+        int Draw();
+        void RemoveCard(int card);
+    }
+}

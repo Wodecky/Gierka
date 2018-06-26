2894ab35c9c157b35adfff393199b3ffd6f65196
Maciej Krebs <maciej.krebs@windowslive.com>
2018-06-26 19:45:24 +0200

    player test

---
+++ GierkaTests/Classes/PlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Gierka.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gierka.Classes.Tests
+{
+    [TestFixture()]
+    public class PlayerTests
+    {
+        [Test()]
+        public void InitializeTurnTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void PlayCardTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
